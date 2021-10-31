de5b32b2d36d0ab1b98a8feceb292ee4644503cb
kala-adhikari <93487648+kala-adhikari@users.noreply.github.com>
2021-10-31 23:33:20 +0545

    Update final_height_measurement.ino

--- final_height_measurement.ino
+++ final_height_measurement.ino
@@ -17,7 +17,7 @@ pinMode(9,OUTPUT);
 pinMode(10,INPUT);
 }
 
-//this runs again and again ultil device shut down
+//this runs again and again until program closes.
 void loop() {
 digitalWrite(9,HIGH);
 delayMicroseconds(20);
